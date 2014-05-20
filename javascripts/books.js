c33250254d92e75fa6ed06bcc46e698704799eb1
Zoltan Biber <zoltan.biber@gmail.com>
2014-05-20 20:33:53 +0100

    refactored code to jquery syntax /// work in progress /// issues with 'hot' class

--- javascripts/books.js
+++ javascripts/books.js
@@ -1,3 +1,17 @@
 function handleResponse(response) {
   console.log(response.items);
-}
+  var image = "";
+  _.each(response.items, function(a) {
+    image = $("<img />");
+    image.attr("src", a.volumeInfo.imageLinks.thumbnail);
+    image.data("title", a.volumeInfo.title);
+    $("#bookshelf").append(image);
+    _.filter(response.items, function(book){
+      if(book.volumeInfo.averageRating > 4) {
+        image.addClass("hot");
+      }
+    });
+  });
+
+
+};
