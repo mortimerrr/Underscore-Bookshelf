65e129515ae74f31cab3712bc698acd855e586c9
Zoltan Biber <zoltan.biber@gmail.com>
2014-05-21 18:54:06 +0100

    put book thumbnail images in a div

--- javascripts/books.js
+++ javascripts/books.js
@@ -2,24 +2,18 @@ function handleResponse(response) {
   console.log(response.items);
   var image = "";
   _.each(response.items, function(book) {
+    image_div = $("<div />");
+    image_div.attr("class", "all-books");
     image = $("<img />");
+    image_div.append(image);
     image.attr("src", book.volumeInfo.imageLinks.thumbnail);
     image.data("title", book.volumeInfo.title);
-    $("#bookshelf").append(image);
+    $("#bookshelf").append(image_div);
     if(book.volumeInfo.averageRating >= 4) {
       image.addClass("hot");
      }
 
   });
-
-    star_image = $("<img />");
-    star_image.addClass("star");
-    star_image.attr({
-      src: "http://img3.wikia.nocookie.net/__cb20100724183918/spongebob/images/3/33/Patrick_Star.svg",
-      width: "20px",
-      height: "20px"
-    }); 
-    $(".hot").append(star_image);
 
   // var hot_books = _.filter(response.items, function(book) {
   //   return book.volumeInfo.averageRating >= 4;
